ead30bea41828ad7df2fa8f81c94c229fd1dc2af
lucykim0921 <kiml696@newschool.edu>
2024-04-07 16:27:02 -0400

    fixed json!!!!

--- js/main.js
+++ js/main.js
@@ -1,45 +1,34 @@
-// Function to render your items
-const renderItems = (data) => {
+const renderItems = (questions) => {
     // The `ul` where the items will be inserted
     const dataList = document.getElementById('data-list');
 
-    // Loop through each item in the data array
-    data.forEach((item) => {
-
+    // Loop through each question in the 'questions' array
+    questions.forEach((question) => {
         // Make a template literal as we have before, inserting your data
         let listItem = `
             <li>
-            <div class="quiz">
-
-                <div class="question">${item.question}</div>
-        
-                <div class="answers">
-                    <button class="option">${item.option1}</button>
-                    <button class="option">${item.option2}</button>
-                    <button class="option">${item.option3}</button>
-                    <button class="option">${item.option4}</button>
-                </div>
-
+                <div class="quiz">
+                    <div class="question">${question.question}</div>
+                    <div class="answers">
+                        ${question.options.map(option => `<button class="option">${option}</button>`).join('')}
+                    </div>
                     <button class="next-button">next</button>
-                    <button class="cross-button"><a href="index.html">&#9747</a><button>
-
-            </div>
+                    <button class="cross-button"><a href="index.html">&#9747</a></button>
+                </div>
             </li>
         `;
-
-
         dataList.insertAdjacentHTML('beforeend', listItem); // Add it to the `ul`!
     });
 };
-
-
 
 // Fetch gets your (local) JSON file…
 fetch('js/data.json')
     .then(response => response.json())
     .then(data => {
-        // And passes the data to the function, above!
-        renderItems(data);
+        // Extract 'questions' array from the JSON response
+        const questions = data.questions;
+        // Render the questions
+        renderItems(questions);
     })
     .catch(error => {
         console.error('Error fetching JSON data:', error);
